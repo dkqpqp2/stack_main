e292b95c00703888ccb8773607f51f95dc09390a
DESKTOP-COA1C72\AR student <test@gmail.com>
2024-05-29 14:25:56 +0900

    로비에서 게임맵 이동 간단 구현  - 캐릭터 및 팀 선택 사항 이동가능.

---
+++ Source/StackOBot/Minigames/GameMap/GamePlayerController.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GamePlayerController.h"
+
+const TSubclassOf<APawn>* const AGamePlayerController::FindCharacterClass(const FString& CharacterClassName) const
+{
+	if (CharacterClassesMap.Contains(CharacterClassName))
+	{
+		return CharacterClassesMap.Find(CharacterClassName);
+	}
+	else
+	{
+		return nullptr;
+	}
+}
