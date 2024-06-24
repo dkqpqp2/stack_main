169fec6202fcbd042cc24645d6a839e47a0a50d3
DESKTOP-COA1C72\AR student <test@gmail.com>
2024-06-24 17:27:20 +0900

    Ammo ui <진행중>

---
+++ Source/StackOBot/ShootingGames/GamMode/ShootingGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Minigames/OBot/GameMode/MG_GameMode.h"
+#include "ShootingGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STACKOBOT_API AShootingGameMode : public AMG_GameMode
+{
+	GENERATED_BODY()
+	
+};
