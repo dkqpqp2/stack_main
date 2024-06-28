a51053dcd836b1622b11484cde3506925a5737fa
DESKTOP-COA1C72\AR student <test@gmail.com>
2024-06-28 17:41:53 +0900

    피니시라인 처리중

--- Source/StackOBot/Minigames/Item/FinishLineBox.h
+++ Source/StackOBot/Minigames/Item/FinishLineBox.h
@@ -19,7 +19,14 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
+	void OnOverlapFinishLine(
+		UPrimitiveComponent* OverlappedComponent, 
+		AActor* OtherActor, 
+		UPrimitiveComponent* OtherComp, 
+		int32 OtherBodyIndex, 
+		bool bFromSweep, 
+		const FHitResult& SweepResult
+	);
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
